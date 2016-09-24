c744acae9902888b91ae73e59f00ccd61384d575
Dieter Brehm <Inkering@gmail.com>
2016-09-24 15:34:26 -0400

    Added hex and dual direction conversion

--- ConversionTesting.py
+++ ConversionTesting.py
@@ -1,7 +1,11 @@
+import base64
+import math
 import baseConversionCore
+
 print("Please input first a number, then a desired base. The output will be that number in your desired base")
-number = int(input("The first number!"))
+number = input("The first number!")
+baseOld = int(input("What base is that in?"))
 baseNew = int(input("What base would you like that in?"))
 
-print(baseConversionCore.toNewBase(number, baseNew))
+print(baseConversionCore.bases(number, baseOld, baseNew))
 
--- baseConversionCore.py
+++ baseConversionCore.py
@@ -1,38 +1,26 @@
-def toNewBase(number, base):
-    result = []
-    while number > 0:
-        result.insert(0, number % base)
-        number = number // base
+def bases(number, oldBase, newBase):
+    decimalVal = toDecimal(number, oldBase)
+    result = toNewBase(decimalVal, newBase)
     return result
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#def fromBase(number, base):
-#    n = 0
-#    for d in number:
-#        n = base * n + d
-#    return n
+def toNewBase(decimalNumber, base):
+    hexVals = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+    result = []
+    count = 0
+    while decimalNumber > 0:
+        result.insert(0, decimalNumber % base)
+        decimalNumber = decimalNumber // base
+        count += 1
+    for index, x in enumerate(result):
+        if x > 9:
+            result[index] = hexVals[x]
+    return result
+def toDecimal(string, base):
+    n = len(string)
+    b = base
+    s = 0
+    chars = list(string)
+    for items in chars:
+        n = n - 1
+        newDecimal = int(items) * (b**n)
+        s = s + newDecimal
+    return s
